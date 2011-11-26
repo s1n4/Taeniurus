374ef2bc5be48b08d078b48002c8c9c1057e2d46
Sina Samavati <contact@s1n4.com>
2011-11-26 18:26:31 +0330

    Twitter module to reading the last tweet of a user

---
+++ modules/twitter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import json, urllib
+
+
+def tweet(username=None) :
+	if not username : username = '_s1n4_'
+	if type(username) is list : username = str(username[0])
+	twitterUrl = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=%s&include_rts=true' % username
+	jsonFile = urllib.urlopen(twitterUrl)
+	return username + ': ' + json.load(jsonFile)[0]['text']
+
