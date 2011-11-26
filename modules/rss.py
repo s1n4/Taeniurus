eb147b213369e461cda4fa5ff91656fd256b4066
Sina Samavati <contact@s1n4.com>
2011-11-26 04:10:22 +0330

    Edited

--- modules/rss.py
+++ modules/rss.py
@@ -4,8 +4,9 @@ import urllib
 def getValue(dom, tag) :
 	return dom.getElementsByTagName(tag)[1].childNodes[0].nodeValue
 
-def rss() :
-	xmlFile = urllib.urlopen('http://s1n4.tumblr.com/rss').read()
+def rss(url=None) :
+	if not url: url = 'http://s1n4.tumblr.com/rss'
+	xmlFile = urllib.urlopen(url).read()
 	dom = minidom.parseString(xmlFile)
 	return getValue(dom, 'title'), getValue(dom, 'link')
 
